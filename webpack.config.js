0130f3927eeb9d376a2aaa85708fa65aade1abad
paulober <44974737+paulober@users.noreply.github.com>
2023-03-21 17:00:01 +0100

    Publish with webpack config

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+//@ts-check
+
+'use strict';
+
+const path = require('path');
+
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+/** @type WebpackConfig */
+const extensionConfig = {
+  target: 'node',
+	mode: 'production',
+
+  entry: './src/index.ts',
+  output: {
+    // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: { },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: "tsconfig.json",
+              compilerOptions: {
+                sourceMap: true,
+              },
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+};
+module.exports = [ extensionConfig ];
